f93d1108d37f1d8b967feb8c5156eae41e2e9351
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2024-05-14 17:03:05 -0300

    fix: background mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -13,22 +12,22 @@ module.exports = {
         'green-main': '#1ED760'
       },
       backgroundImage: theme => ({
-        'spotify-theme': "url('/img/bursts.svg')",
-        'spotify-theme-mobile': "url('/img/bursts-mobile.svg')"
-      }),
+      "spotify-theme": "url('../img/bursts.svg')",
+      "spotify-theme-mobile": "url('../img/bursts-mobile.svg')",
+   }),
       backgroundSize: {
-        '175%' : '175%',
-        '195%' : '195%'
+        '175%': '175%',
+        '195%': '195%'
       },
       backgroundPosition: {
         'banner': '46% 4%',
-        'banner-mobile' : 'top 25% center'
+        'banner-mobile': 'top 25% center'
       },
       fontSize: {
         '9xl': '9rem'
       }
     },
   },
+  variants: {},
   plugins: [],
 }
-
